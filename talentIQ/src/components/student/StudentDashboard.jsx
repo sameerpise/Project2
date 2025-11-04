be50eea8163b96592560a38ae9c2000c83943ff4
Sameer Laxman PIse <79313336+sameerpise@users.noreply.github.com>
2025-11-04 16:22:15 +0530

    Update StudentDashboard.jsx

--- talentIQ/src/components/student/StudentDashboard.jsx
+++ talentIQ/src/components/student/StudentDashboard.jsx
@@ -149,6 +149,7 @@ export default function StudentDashboard() {
     // background:
     //   "linear-gradient(135deg, rgba(255,255,255,0.25), rgba(255,255,255,0.05))",
      background: "linear-gradient(90deg, #f6ae22 0%, #ff7f50 50%, #050505 100%)",
+   // background: linear-gradient(90deg, #f6ae22 0%, #ff7f50 50%, #050505 100%);
     backgroundSize: "400% 400%",
     // animation: "gradientShift 8s ease infinite",
     p: 1,
