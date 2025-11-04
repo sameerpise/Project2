0b932dff1dc2c9ad982a731fe132c3d4a7ceb3d7
Sameer Laxman PIse <79313336+sameerpise@users.noreply.github.com>
2025-11-04 15:39:20 +0530

    Update StudentDashboard.jsx

--- talentIQ/src/components/student/StudentDashboard.jsx
+++ talentIQ/src/components/student/StudentDashboard.jsx
@@ -152,7 +152,7 @@ export default function StudentDashboard() {
     animation: "gradientShift 8s ease infinite",
     p: 1,
     backdropFilter: "blur(12px)",
-    borderBottom: "1px solid rgba(255,255,255,0.1)",
+    // borderBottom: "1px solid rgba(255,255,255,0.1)",
     // boxShadow: "0 4px 20px rgba(0,0,0,0.3)",
     zIndex: (theme) => theme.zIndex.drawer + 1,
     "@keyframes gradientShift": {
